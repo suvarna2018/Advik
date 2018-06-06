889583f3c3589fbe3a5714fde7b6d13a3b77b76d
suvarna2018 <suvarnashankarp@gmail.com>
2018-06-06 10:43:24 +0530

    Added message

---
+++ welcome/src/com/sbi/loans/homeloans/TenthClass.java
@@ -0,0 +1,12 @@
+package com.sbi.loans.homeloans;
+
+public class TenthClass {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi");
+
+	}
+
+}
